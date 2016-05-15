94ea8004869a4a32d7a3dd6ef110ed641ec86107
Amruth Pillai <im.amruth@gmail.com>
2016-05-15 13:41:31 +0530

    Update CalculateNCR.asm

--- 4th-Sem/Microprocessor_Lab/8a-CalculateNCR/CalculateNCR.asm
+++ 4th-Sem/Microprocessor_Lab/8a-CalculateNCR/CalculateNCR.asm
@@ -1,52 +1,71 @@
 ; Assembly Level Program 9a
 ; Compute nCr using recursive procedure. Assume that ‘n’ and ‘r’ are non-negative integers.
 
-.model small
+.model SMALL
 
 .data
-n db 05h
-r db 02h
-ncr dw ?
+	N	dB	21d
+	R	dB	19d
+	NCR	dW	?
 
 .code
-start:  mov ax,@data
-           mov ds,ax
-           mov ax,00h
-           mov al,n
-           mov bl,r
-           mov ncr,00h
-           call ncrp
-           mov ah,4ch
-           int 21h
-        
-           ncrp proc
-           cmp ax,bx
-           je pls1
-           cmp bx,00h
-           je pls1
-           cmp bx,01h
-           je plsn
-           dec ax
-           cmp ax,bx
-           je pls
-           push ax
-           push bx
-           call ncrp
-           pop bx
-           pop ax
-           dec bx
-           push ax
-           push bx
-           call ncrp
-           pop bx
-           pop ax
-           ret
-pls1:   inc ncr
-           ret
-plsn:   add ncr,ax
-           ret
-pls:     add ncr,ax
-           inc ncr
-           ret
-           ncrp endp
-end start
+	; Initialize Data Segment
+	MOV AX, @DATA
+	MOV DS, AX
+	
+	; Clear NCR
+	MOV AX, 00h
+	MOV AL, N
+	MOV BL, R
+	MOV NCR, 00h
+	
+	CALL NCRProcedure
+	
+Exit:
+	MOV AH, 4Ch
+	INT 21h
+	
+NCRProcedure PROC NEAR
+	CMP AX, BX
+	JE IncrementBy1
+	
+	CMP BX, 00h
+	JE IncrementBy1
+	
+	CMP BX, 01h
+	JE IncrementByN
+	
+	DEC AX
+	
+	CMP AX, BX
+	JE IncrementByNPlus1
+	
+	PUSH AX
+	PUSH BX
+	CALL NCRProcedure
+	POP BX
+	POP AX
+	
+	DEC BX
+	
+	PUSH AX
+	PUSH BX
+	CALL NCRProcedure
+	POP BX
+	POP AX
+	RET
+	
+IncrementBy1:
+	INC NCR
+	RET
+	
+IncrementByN:
+	ADD NCR, AX
+	RET
+	
+IncrementByNPlus1:
+	ADD NCR, AX
+	INC NCR
+	RET
+NCRProcedure ENDP
+END
