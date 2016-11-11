ca44352e84ec542ef24f4b857d3318a638e656a8
Varsha Jambunathan <jvarsha621@gmail.com>
2016-11-11 23:52:43 +0530

    Update query5.sql
    Modifying the change in price wrt the Publisher.

--- 5th-Sem/DataBaseManagementSystem_Lab/04_BookDealer/query5.sql
+++ 5th-Sem/DataBaseManagementSystem_Lab/04_BookDealer/query5.sql
@@ -3,8 +3,24 @@
 UPDATE  Catalog 
 SET price = (1.1) * price
 WHERE authorid = ( 
-			SELECT authorid
-			FROM Author 
-			WHERE name = 'NAVATHE'
+			SELECT publisherid
+			FROM Publisher
+			WHERE name = 'JOHN WILEY'
 		)
 ;
+
+
+--QUERY
+SELECT bookid, title, price, price*1.1
+FROM Catalog
+WHERE publisherid IN (
+			SELECT publisherid
+			FROM Publisher
+			WHERE name='JOHN WILEY'
+		     )
+;
+
+--OUTPUT
+
+--BOOKID 	TITLE			PRICE		PRICE*1.1
+--2	    PRINCIPLES OF DBMS		400		440
