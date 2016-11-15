c10394f618d4ae65d4e4f3ee948500f71e3409bb
Amruth Pillai <im.amruth@gmail.com>
2016-11-15 22:23:22 +0530

    Update query3.sql

--- 5th-Sem/DataBaseManagementSystem_Lab/04_BookDealer/query3.sql
+++ 5th-Sem/DataBaseManagementSystem_Lab/04_BookDealer/query3.sql
@@ -1,21 +1,17 @@
--- Give the details of the authors who have 2 or more books in the catalog and the price of the books is greater
--- than the average price of the books in the catalog and the year of publication is after 2000.
+-- Give the details of the authors who have 2 or more books in the catalog and the price of the books is greater than the average price of the books in the catalog and the year of publication is after 2000.
 
 SELECT *
 FROM Author A
 WHERE EXISTS (
-		SELECT A1.authorid, COUNT(A1.authorid)
-		FROM Author A1, Catalog C
-		WHERE 
-			A1.authorid = C.authorid AND
-			A.authorid = A1.authorid AND
-			C.yearofpublish > 2000 AND
-			C.price > (
-					SELECT AVG(price)
-					FROM Catalog
-				)
-		GROUP BY A1.authorid
-		HAVING COUNT(A1.authorid) >= 2
-	)
-;
-
+	SELECT A1.authorid, COUNT(A1.authorid)
+	FROM Author A1, Book_Catalog C
+	WHERE A1.authorid = C.authorid AND
+		A.authorid = A1.authorid AND
+		C.yearofpublish > 2000 AND
+		C.price > (
+			SELECT AVG(price)
+			FROM Book_Catalog
+		)
+	GROUP BY A1.authorid
+	HAVING COUNT(A1.authorid) >= 2
+);
