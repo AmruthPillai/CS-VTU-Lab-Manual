6934ec13f4fba403b0babd22a420ddc8c360e27f
Amruth Pillai <im.amruth@gmail.com>
2016-11-15 20:35:36 +0530

    Update query3.sql

--- 5th-Sem/DataBaseManagementSystem_Lab/03_StudentCoursesBooks/query3.sql
+++ 5th-Sem/DataBaseManagementSystem_Lab/03_StudentCoursesBooks/query3.sql
@@ -1,5 +1,4 @@
--- Demonstrate how you add a new text book to the database and make this book be adopted by some
--- department.
+-- Demonstrate how you add a new text book to the database and make this book be adopted by some department.
 
 INSERT INTO Text VALUES (11, 'DATABASE FUNDAMENTALS', 'TATA MCGRAW HILL', 'SCHIELD');
-INSERT INTO BookAdoption VALUES (1, 3, 11);
+INSERT INTO Book_Adoption VALUES (1, 3, 11);
