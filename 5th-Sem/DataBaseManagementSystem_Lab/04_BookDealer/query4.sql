f018dbd437fd1496630e212f76e6791007df4dc2
Amruth Pillai <im.amruth@gmail.com>
2016-11-15 22:37:49 +0530

    Update query4.sql

--- 5th-Sem/DataBaseManagementSystem_Lab/04_BookDealer/query4.sql
+++ 5th-Sem/DataBaseManagementSystem_Lab/04_BookDealer/query4.sql
@@ -1,16 +1,10 @@
 -- Find the author of the book which has maximum sales.
 
 SELECT A.name
-FROM Author A
-WHERE A.authorid IN (
-	SELECT BC.authorid
-	FROM Book_Catalog BC
-	WHERE BC.bookid IN (
-		SELECT OD.bookid
+FROM Author A, Book_Catalog BC, Order_Details OD
+WHERE A.authorid = BC.authorid AND
+	BC.bookid = OD.bookid AND
+	OD.quantity = (
+		SELECT MIN(OD.quantity)
 		FROM Order_Details OD
-		WHERE OD.quantity = (
-			SELECT MAX(OD.quantity)
-			FROM Order_Details OD
-		)
-	)
-);
+	);
