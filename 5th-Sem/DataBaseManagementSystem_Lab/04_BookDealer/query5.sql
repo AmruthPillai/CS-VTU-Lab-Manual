dc9a25ba2cbe4d6656d904c5840079cd5535dba3
Amruth Pillai <im.amruth@gmail.com>
2016-11-15 22:42:54 +0530

    Update query5.sql

--- 5th-Sem/DataBaseManagementSystem_Lab/04_BookDealer/query5.sql
+++ 5th-Sem/DataBaseManagementSystem_Lab/04_BookDealer/query5.sql
@@ -1,26 +1,9 @@
 -- Demonstrate how you increase the price of books published by a specific publisher by 10%.
 
-UPDATE  Catalog 
-SET price = (1.1) * price
-WHERE authorid = ( 
-			SELECT publisherid
-			FROM Publisher
-			WHERE name = 'JOHN WILEY'
-		)
-;
-
-
---QUERY
-SELECT bookid, title, price, price*1.1
-FROM Catalog
-WHERE publisherid IN (
-			SELECT publisherid
-			FROM Publisher
-			WHERE name='JOHN WILEY'
-		     )
-;
-
---OUTPUT
-
---BOOKID 	TITLE			PRICE		PRICE*1.1
---2	    PRINCIPLES OF DBMS		400		440
+SELECT BC.bookid, BC.title, BC.price, price*1.1
+FROM Book_Catalog BC
+WHERE BC.publisherid IN (
+	SELECT P.publisherid
+	FROM Publisher P
+	WHERE P.name='JOHN WILEY'
+);
