5e36986b6627985645309f2749a15b623e8f7a51
Amruth Pillai <im.amruth@gmail.com>
2016-11-15 18:13:48 +0530

    Update query2.sql

--- 5th-Sem/DataBaseManagementSystem_Lab/01_StudentClass/query2.sql
+++ 5th-Sem/DataBaseManagementSystem_Lab/01_StudentClass/query2.sql
@@ -1,13 +1,8 @@
 -- Find the names of all classes that either meet in room R128 or have five or more Students enrolled.
-
-SELECT DISTINCT C.cname 
+SELECT DISTINCT C.cname
 FROM Class C
-WHERE
-	C.room = 'R128' OR
-	C.cname IN (
-			SELECT E.cname
-			FROM Enrolled E
-			GROUP BY E.cname
-			HAVING COUNT(*) >= 5
-		)
-;
+WHERE C.room = 'R128' OR
+	C.cname IN (SELECT E.cname
+		FROM Enrolled E
+		GROUP BY E.cname
+		HAVING COUNT(*) >= 5);
