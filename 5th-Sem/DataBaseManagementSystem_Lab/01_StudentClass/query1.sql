94fe8f7f486d8dc360ab14d9baefb3b10c58e68f
Amruth Pillai <im.amruth@gmail.com>
2016-11-15 18:05:40 +0530

    Update query1.sql

--- 5th-Sem/DataBaseManagementSystem_Lab/01_StudentClass/query1.sql
+++ 5th-Sem/DataBaseManagementSystem_Lab/01_StudentClass/query1.sql
@@ -2,11 +2,8 @@
 
 SELECT DISTINCT S.sname
 FROM Student S, Enrolled E, Class C, Faculty F
-WHERE 
-	S.snum = E.snum AND 
+WHERE S.snum = E.snum AND 
 	E.cname = C.cname AND 
 	C.fid = F.fid AND
 	F.fname = 'Prof. Harshith' AND 
-	S.slevel = 'JR'
-;
-		
+	S.slevel = 'JR';
