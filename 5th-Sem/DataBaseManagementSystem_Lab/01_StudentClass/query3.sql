90e9b48147c3ab457fd7c9e9e46c5831c7c979b3
Amruth Pillai <im.amruth@gmail.com>
2016-11-15 18:22:37 +0530

    Update query3.sql

--- 5th-Sem/DataBaseManagementSystem_Lab/01_StudentClass/query3.sql
+++ 5th-Sem/DataBaseManagementSystem_Lab/01_StudentClass/query3.sql
@@ -2,16 +2,10 @@
 
 SELECT DISTINCT S.sname
 FROM Student S
-WHERE S.snum IN (
-			SELECT E1.snum
-			FROM Enrolled E1, Enrolled E2, Class C1, Class C2
-			WHERE 
-				E1.snum = E2.snum AND 
-				E1.cname <> E2.cname AND
-				E1.cname = C1.cname AND 
-				E2.cname = C2.cname AND 
-				C1.meets_at = C2.meets_at
-		)
-;
-
-		
+WHERE S.snum IN (SELECT E1.snum
+	FROM Enrolled E1, Enrolled E2, Class C1, Class C2
+	WHERE E1.cname = C1.cname AND
+		E2.cname = C2.cname AND
+		E1.snum = E2.snum AND
+		C1.cname <> C2.cname AND
+		C1.meets_at = C2.meets_at);
