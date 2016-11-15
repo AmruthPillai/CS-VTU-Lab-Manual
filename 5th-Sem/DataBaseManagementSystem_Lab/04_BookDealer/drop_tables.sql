1a0119f0024ce0e43cbfaa12c677c4c43ef6ba96
Amruth Pillai <im.amruth@gmail.com>
2016-11-15 22:51:46 +0530

    Create drop_tables.sql

---
+++ 5th-Sem/DataBaseManagementSystem_Lab/04_BookDealer/drop_tables.sql
@@ -0,0 +1,7 @@
+-- Drop all the tables in reverse order of their creation
+
+DROP TABLE Order_Details;
+DROP TABLE Book_Catalog;
+DROP TABLE Book_Category;
+DROP TABLE Publisher;
+DROP TABLE Author;
