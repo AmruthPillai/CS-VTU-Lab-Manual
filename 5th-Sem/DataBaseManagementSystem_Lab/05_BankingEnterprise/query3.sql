641dc4d38a6baa99d1f0675f51746f10f0727e7b
Amruth Pillai <im.amruth@gmail.com>
2016-11-15 23:12:23 +0530

    Update query3.sql

--- 5th-Sem/DataBaseManagementSystem_Lab/05_BankingEnterprise/query3.sql
+++ 5th-Sem/DataBaseManagementSystem_Lab/05_BankingEnterprise/query3.sql
@@ -1,17 +1,13 @@
 -- Find all the customers who have at least two accounts at the Main branch.
 
 SELECT *
-FROM BankCustomer C
+FROM Customer C
 WHERE EXISTS (
-		SELECT D.customername, COUNT(D.customername)
-		FROM Depositer D, BankAccount BA
-		WHERE 
-			D.accno = BA.accno AND
-			C.customername = D.customername AND
-			BA.branchname = 'RESIDENCY ROAD'
-
-		GROUP BY D.customername
-		HAVING COUNT(D.customername) >= 2
-	)
-;
-
+	SELECT D.customername, COUNT(D.customername)
+	FROM Depositor D, Account A
+	WHERE D.accno = A.accno AND
+		C.customername = D.customername AND
+		A.branchname = 'RESIDENCY ROAD'
+	GROUP BY D.customername
+	HAVING COUNT(D.customername) >= 2
+);
