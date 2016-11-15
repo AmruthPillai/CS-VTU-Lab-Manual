1f02c6ac265bc0c1f94d26f99319da4f9adfa672
Amruth Pillai <im.amruth@gmail.com>
2016-11-15 20:53:24 +0530

    Update query5.sql

--- 5th-Sem/DataBaseManagementSystem_Lab/03_StudentCoursesBooks/query5.sql
+++ 5th-Sem/DataBaseManagementSystem_Lab/03_StudentCoursesBooks/query5.sql
@@ -1,18 +1,14 @@
 -- List any department that has all its adopted books published by a specific publisher.
 
 SELECT C.dept, T.booktitle, T.publisher 
-FROM Course C, Text T, BookAdoption BA 
-WHERE 
-	C.course = BA.course AND 
+FROM Course C, Text T, Book_Adoption BA 
+WHERE C.courseno = BA.courseno AND 
 	T.bookisbn = BA.bookisbn AND
-	T.publisher = 'PEARSON'AND
+	T.publisher = 'PEARSON' AND
 	T.publisher = ALL (
-				SELECT T1.publisher 
-				FROM Course C1, BookAdoption BA1, Text T1 
-				WHERE 
-					BA1.bookisbn = T1.bookisbn AND 
-					BA1.course = C1.course AND 
-					C.dept = C1.dept
-			)
-; 
-
+		SELECT T1.publisher 
+		FROM Course C1, Book_Adoption BA1, Text T1 
+		WHERE BA1.bookisbn = T1.bookisbn AND
+			BA1.courseno = C1.courseno AND 
+			C.dept = C1.dept
+	);
