97be82b0a22738d5e5f20d648edb49f15b1003a9
Amruth Pillai <im.amruth@gmail.com>
2016-11-15 22:59:22 +0530

    Create drop_tables.sql

---
+++ 5th-Sem/DataBaseManagementSystem_Lab/05_BankingEnterprise/drop_tables.sql
@@ -0,0 +1,8 @@
+-- Drop all the tables in reverse order of their creation
+
+DROP TABLE Borrower;
+DROP TABLE Loan;
+DROP TABLE Depositor;
+DROP TABLE Customer;
+DROP TABLE Account;
+DROP TABLE Branch;
