3874d179a78f4b3d961c1fd975dca7a36ec2e23d
Amruth Pillai <im.amruth@gmail.com>
2016-11-29 19:00:38 +0530

    updated program with better readme

--- 5th-Sem/SystemSoftware_Lab/12_BankersAlgorithm/12BankersAlgorithm.c
+++ 5th-Sem/SystemSoftware_Lab/12_BankersAlgorithm/12BankersAlgorithm.c
@@ -1,11 +1,3 @@
-## Aim:
-Design, develop and run a program to implement the Bankers Algorithm. Demonstrate its working with different data values.
-
-## Description:
-The Banker's algorithm is a resource allocation and deadlock avoidance algorithm developed by Edsger Dijkstra that tests for safety by simulating the allocation of predetermined maximum possible amounts of all resources, and then makes an "s-state" check to test for possible deadlock conditions for all other pending activities, before deciding whether allocation should be allowed to continue. The Banker's algorithm is run by the operating system whenever a process requests resources. The algorithm avoids deadlock by denying or postponing the request if it determines that accepting the request could put the system in an unsafe state. When a new process enters a system, it must declare the maximum number of instances of each resource type that it may ever claim; clearly, that number may not exceed the total number of resources in the system. Also, when a process gets all its requested resources it must return them in a finite amount of time.
-
-## Code
-```
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -121,39 +113,3 @@ void releaseresource() {
 	for(j=0;j<n;j++)
 		work[j]=work[j]+alloc[process][j];
 }
-```
-
-## Output
-```
-Enter the no. of processes:
-5
-Enter the no. of resources:
-3
-Enter the Allocation Matrix:
-0 1 0
-2 0 0
-3 0 2
-2 1 1
-0 0 2
-Enter the Maximum Matrix:
-7 5 3
-3 2 2
-9 0 2
-2 2 2
-4 3 3
-Enter the Available Vector:
-3 3 2
-Need Matrix is:
-7       4       3
-1       2       2
-6       0       0
-0       1       1
-4       3       1
-1 executed!
-3 executed!
-0 executed!
-2 executed!
-4 executed!
-Safe Sequence is:
-1       3       0       2       4
-```
