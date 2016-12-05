59d9493fd1bf7bd25904e280423bf548c7d36f82
Prabhubalu <im.prabhub@gmail.com>
2016-12-05 20:00:39 +0530

    long live amma

--- 5th-Sem/SystemSoftware_Lab/08b_FileHandle/08bFileHandle.c
+++ 5th-Sem/SystemSoftware_Lab/08b_FileHandle/08bFileHandle.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
 
 int main(int argc, char *argv[]) {
-	int fd;
+	int file;
 	char buf1[16] = "ABCDEFGHIJKLMNOP";
 	char buf2[16] = "abcdefghijklmnop";
 
-	fd = open("data.txt", O_RDWR|O_CREAT|O_EXCL, 0765);
-	
-	if (fd == -1)
-		printf("Error creating file.\n");
-	else
-		write(fd, buf1, 16);
-	lseek(fd, 32, SEEK_CUR);
-	write(fd, buf2, 16);
-	system("vi data.txt");
-	close(fd);
+	file = open("data.txt", O_RDWR|O_CREAT|O_EXCL);
+
+	write(file, buf1, 16);
+	lseek(file, 32, SEEK_CUR);
+	write(file, buf2, 16);
+
+	system("od -bc data.txt");
+	close(file);
+
+	return 0;
 }
