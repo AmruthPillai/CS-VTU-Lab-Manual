f60cdce9fa6a42db6019d587743e8622960c14cd
Prabhubalu <im.prabhub@gmail.com>
2016-12-05 20:28:52 +0530

    9b - child making factory

--- 5th-Sem/SystemSoftware_Lab/09b_ChildProcessIDs/09bChildProcessIDs.c
+++ 5th-Sem/SystemSoftware_Lab/09b_ChildProcessIDs/09bChildProcessIDs.c
@@ -3,17 +3,14 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-	pid_t p;
+	int p = fork();
 	
-	if ((p = fork()) < 0) {
-		printf("Fork Error.\n");
-		return 0;
-	} else if (p == 0) {
+	if (p == 0) {
 		printf("I am Child Process.\n");
 		printf("My Parent Process ID is: %d\n", getppid());
 		printf("My Process ID is: %d\n", getpid());
 	} else {
-		wait();
+		wait(0);
 		printf("I am Parent Process.\n");
 		printf("My Child Process ID is: %d\n", p);
 		printf("My Process ID is: %d\n", getpid());
