d4bc2a8035d73258b29443f121510ac125b9fe7d
Rohan Kumar <rohan.carmel@gmail.com>
2016-12-06 14:32:23 +0530

    php programs

--- 7th-Sem/ProgrammingTheWeb_Lab/lab9.php
+++ 7th-Sem/ProgrammingTheWeb_Lab/lab9.php
@@ -1,14 +1,18 @@
 <?php
+//current time + 20 seconds
 $inTwentySeconds = 20  + time();
 
+//if cookies are present
 if(isset($_COOKIE['lastVisit']))
 {
+	//get the value from the cookie
 	$visit = $_COOKIE['lastVisit'];
   echo "Your last visit was - ". $visit;
 }
-
+//if cookie is expired
 else
 {
+	//setcookie(name,value,expire)
   setcookie('lastVisit', date("h:i:s A - m/d/y"), $inTwentySeconds);
   echo "You've got some expired  cookies!";
 }
