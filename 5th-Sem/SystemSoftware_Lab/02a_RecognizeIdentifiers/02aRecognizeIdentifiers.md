e7e05fda31faebefbb25737e55adc9b92dacb2f0
Amruth Pillai <im.amruth@gmail.com>
2016-12-08 15:25:23 +0530

    2a two way

--- 5th-Sem/SystemSoftware_Lab/02a_RecognizeIdentifiers/02aRecognizeIdentifiers.md
+++ 5th-Sem/SystemSoftware_Lab/02a_RecognizeIdentifiers/02aRecognizeIdentifiers.md
@@ -1,64 +1,95 @@
-##AIM:
-Program to recognize a valid arithmetic expression and to recognize 
-the identifiers and operators present. Print them separately.
+## Aim
+Program to recognize a valid arithmetic expression and to recognize the identifiers and operators present. Print them separately.
 
-##DESCRIPTION:
+## Description
 Following the syntax for defining an identifier we generate arithmetic expressions using basic arithmetic operators such as +, -, * & /.
 
-##FLEX:  flex - the fast lexical analyser generator.
-
-##CODE:
-<pre>%{
-`#`include<stdio.h>
-int a[4]={0,0,0,0};
-int valid=1,i;
-int nop=0;
+## Code
+```
+%{
+int op_count, br_count, id_count, a[4] = {0, 0, 0, 0};
 %}
-%x OPER
+
 %%
-[a-zA-Z0-9]+ {BEGIN OPER;nop++;}
-<OPER>"+" {if(valid) {valid=0;i=0;}else ext();}
-<OPER>"-" {if(valid) {valid=0;i=1;}else ext();}
-<OPER>"*" {if(valid) {valid=0;i=2;}else ext();}
-<OPER>"/" {if(valid) {valid=0;i=3;}else ext();}
-<OPER>[a-zA-Z0-9]+ {nop++;if(valid==0){valid=1;a[i]++;}else ext();}
-<OPER>. {if(valid){valid=0;ext();}else return 0;}
-<OPER>"\n" {if(valid==0){ext();}else return 0;}
-. ext();
-\n ext();
+
+"+" { a[0]++; }
+"-" { a[1]++; }
+"*" { a[2]++; }
+"/" { a[3]++; }
+"(" { br_count++; }
+")" { br_count--; }
+[a-zA-Z0-9]+ { id_count++; printf("Identifier Found: %s\n", yytext); }
+\n { return 0; }
+
 %%
-int ext()
-{
-	printf("Invalid expression\n");
-	exit(0);
-}
-int main()
-{
-	printf("Enter expression\n");
+
+int main(int argc, char *argv[]) {
+	printf("Enter the expression: ");
+
 	yylex();
-	printf("Expression is valid\n");
-	printf("No. of '+' is:%d\n",a[0]);
-	printf("No. of '-' is:%d\n",a[1]);
-	printf("No. of '*' is:%d\n",a[2]);
-	printf("No. of '/' is:%d\n",a[3]);
-	printf("No. of operands is:%d\n",nop);
+
+	printf("\nOperator Count:\n");
+	printf("Addition:\t%d\nSubtraction:\t%d\nMultiplication:\t%d\nDivision:\t%d\n", a[0], a[1], a[2], a[3]);
+	printf("Number of Identifiers:\t%d\n", id_count);
+
+	op_count = a[0] + a[1] + a[2] + a[3];
+
+	if (op_count >= id_count || br_count != 0 || id_count == 1)
+		printf("\nIt is an invalid expression!\n");
+	else
+		printf("\nIt is a valid expression!\n");
+
 	return 0;
-}</pre>		
+}
 
-##COMPILE:
-<pre>lex 2a.l
-gcc lex.yy.c -ll
-./a.out </pre>
+int yywrap() {
+	return 1;
+}
+```
 
-##OUTPUT:
-<pre>Enter expression
-2+4*6+2/5-3*6     
-Expression is valid
-No. of '+' is:2
-No. of '-' is:1
-No. of '*' is:2
-No. of '/' is:1
-No. of operands is:7
-Enter expression
-2-3*
-Invalid expression</pre>
+## Execution
+```
+lex 02aRecognizeIdentifiers.l
+gcc lex.yy.c
+./a.out
+```
+
+## Output
+#### Test Case 1: Valid
+```
+Enter the expression: a+b-c*(d/e)/f-g+h
+Identifier Found: a
+Identifier Found: b
+Identifier Found: c
+Identifier Found: d
+Identifier Found: e
+Identifier Found: f
+Identifier Found: g
+Identifier Found: h
+
+Operator Count:
+Addition:       2
+Subtraction:    2
+Multiplication: 2
+Division:       2
+Number of Identifiers:  8
+
+It is a valid expression!
+```
+
+#### Test Case 2: Invalid
+```
+Enter the expression: a+b-c/
+Identifier Found: a
+Identifier Found: b
+Identifier Found: c
+
+Operator Count:
+Addition:       1
+Subtraction:    1
+Multiplication: 0
+Division:       1
+Number of Identifiers:  3
+
+It is an invalid expression!
+```
--- 5th-Sem/SystemSoftware_Lab/02a_RecognizeIdentifiers/lex.yy.c
+++ 5th-Sem/SystemSoftware_Lab/02a_RecognizeIdentifiers/lex.yy.c
@@ -8,7 +8,7 @@
 #define FLEX_SCANNER
 #define YY_FLEX_MAJOR_VERSION 2
 #define YY_FLEX_MINOR_VERSION 5
-#define YY_FLEX_SUBMINOR_VERSION 35
+#define YY_FLEX_SUBMINOR_VERSION 37
 #if YY_FLEX_SUBMINOR_VERSION > 0
 #define FLEX_BETA
 #endif
@@ -46,7 +46,6 @@ typedef int16_t flex_int16_t;
 typedef uint16_t flex_uint16_t;
 typedef int32_t flex_int32_t;
 typedef uint32_t flex_uint32_t;
-typedef uint64_t flex_uint64_t;
 #else
 typedef signed char flex_int8_t;
 typedef short int flex_int16_t;
@@ -54,7 +53,6 @@ typedef int flex_int32_t;
 typedef unsigned char flex_uint8_t; 
 typedef unsigned short int flex_uint16_t;
 typedef unsigned int flex_uint32_t;
-#endif /* ! C99 */
 
 /* Limits of integral types. */
 #ifndef INT8_MIN
@@ -84,6 +82,8 @@ typedef unsigned int flex_uint32_t;
 #ifndef UINT32_MAX
 #define UINT32_MAX             (4294967295U)
 #endif
+
+#endif /* ! C99 */
 
 #endif /* ! FLEXINT_H */
 
@@ -355,13 +355,13 @@ static void yy_fatal_error (yyconst char msg[]  );
  */
 #define YY_DO_BEFORE_ACTION \
 	(yytext_ptr) = yy_bp; \
-	yyleng = (yy_size_t) (yy_cp - yy_bp); \
+	yyleng = (size_t) (yy_cp - yy_bp); \
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
 
-#define YY_NUM_RULES 8
-#define YY_END_OF_BUFFER 9
+#define YY_NUM_RULES 9
+#define YY_END_OF_BUFFER 10
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -369,28 +369,28 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static yyconst flex_int16_t yy_accept[14] =
+static yyconst flex_int16_t yy_accept[15] =
     {   0,
-        0,    0,    9,    8,    5,    6,    3,    1,    2,    4,
-        7,    7,    0
+        0,    0,   10,    9,    8,    5,    6,    3,    1,    2,
+        4,    7,    7,    0
     } ;
 
 static yyconst flex_int32_t yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
-        3,    4,    5,    1,    6,    1,    7,    8,    8,    8,
-        8,    8,    8,    8,    8,    8,    8,    1,    1,    1,
-        1,    1,    1,    1,    8,    8,    8,    8,    8,    8,
-        8,    8,    8,    8,    8,    8,    8,    8,    8,    8,
-        8,    8,    8,    8,    8,    8,    8,    8,    8,    8,
-        1,    1,    1,    1,    1,    1,    8,    8,    8,    8,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    3,
+        4,    5,    6,    1,    7,    1,    8,    9,    9,    9,
+        9,    9,    9,    9,    9,    9,    9,    1,    1,    1,
+        1,    1,    1,    1,    9,    9,    9,    9,    9,    9,
+        9,    9,    9,    9,    9,    9,    9,    9,    9,    9,
+        9,    9,    9,    9,    9,    9,    9,    9,    9,    9,
+        1,    1,    1,    1,    1,    1,    9,    9,    9,    9,
 
-        8,    8,    8,    8,    8,    8,    8,    8,    8,    8,
-        8,    8,    8,    8,    8,    8,    8,    8,    8,    8,
-        8,    8,    1,    1,    1,    1,    1,    1,    1,    1,
+        9,    9,    9,    9,    9,    9,    9,    9,    9,    9,
+        9,    9,    9,    9,    9,    9,    9,    9,    9,    9,
+        9,    9,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -407,33 +407,35 @@ static yyconst flex_int32_t yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst flex_int32_t yy_meta[9] =
+static yyconst flex_int32_t yy_meta[10] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1
+        1,    1,    1,    1,    1,    1,    1,    1,    1
     } ;
 
-static yyconst flex_int16_t yy_base[14] =
+static yyconst flex_int16_t yy_base[15] =
     {   0,
-        0,    0,   11,   12,   12,   12,   12,   12,   12,   12,
-        2,    1,   12
+        0,    0,   12,   13,   13,   13,   13,   13,   13,   13,
+       13,    2,    1,   13
     } ;
 
-static yyconst flex_int16_t yy_def[14] =
+static yyconst flex_int16_t yy_def[15] =
     {   0,
-       13,    1,   13,   13,   13,   13,   13,   13,   13,   13,
-       13,   13,    0
+       14,    1,   14,   14,   14,   14,   14,   14,   14,   14,
+       14,   14,   14,    0
     } ;
 
-static yyconst flex_int16_t yy_nxt[21] =
+static yyconst flex_int16_t yy_nxt[23] =
     {   0,
-        4,    5,    6,    7,    8,    9,   10,   11,   12,   12,
-       13,    3,   13,   13,   13,   13,   13,   13,   13,   13
+        4,    5,    6,    7,    8,    9,   10,   11,   12,   13,
+       13,   14,    3,   14,   14,   14,   14,   14,   14,   14,
+       14,   14
     } ;
 
-static yyconst flex_int16_t yy_chk[21] =
+static yyconst flex_int16_t yy_chk[23] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,   12,   11,
-        3,   13,   13,   13,   13,   13,   13,   13,   13,   13
+        1,    1,    1,    1,    1,    1,    1,    1,    1,   13,
+       12,    3,   14,   14,   14,   14,   14,   14,   14,   14,
+       14,   14
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -452,8 +454,8 @@ int yy_flex_debug = 0;
 char *yytext;
 #line 1 "02aRecognizeIdentifiers.l"
 #line 2 "02aRecognizeIdentifiers.l"
-int op_count, br_count, id_count, a[4] = {0,0,0,0};
-#line 457 "lex.yy.c"
+int op_count, br_count, id_count, a[4] = {0, 0, 0, 0};
+#line 459 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -464,7 +466,7 @@ int op_count, br_count, id_count, a[4] = {0,0,0,0};
  */
 #include <unistd.h>
 #endif
-
+    
 #ifndef YY_EXTRA_TYPE
 #define YY_EXTRA_TYPE void *
 #endif
@@ -542,7 +544,7 @@ static int input (void );
 /* This used to be an fputs(), but since the string might contain NUL's,
  * we now use fwrite().
  */
-#define ECHO fwrite( yytext, yyleng, 1, yyout )
+#define ECHO do { if (fwrite( yytext, yyleng, 1, yyout )) {} } while (0)
 #endif
 
 /* Gets input and stuffs it into "buf".  number of characters read, or YY_NULL,
@@ -553,7 +555,7 @@ static int input (void );
 	if ( YY_CURRENT_BUFFER_LVALUE->yy_is_interactive ) \
 		{ \
 		int c = '*'; \
-		yy_size_t n; \
+		size_t n; \
 		for ( n = 0; n < max_size && \
 			     (c = getc( yyin )) != EOF && c != '\n'; ++n ) \
 			buf[n] = (char) c; \
@@ -635,9 +637,10 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
     
-#line 4 "02aRecognizeIdentifiers.l"
+#line 5 "02aRecognizeIdentifiers.l"
 
-#line 641 "lex.yy.c"
+
+#line 644 "lex.yy.c"
 
 	if ( !(yy_init) )
 		{
@@ -690,13 +693,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 14 )
+				if ( yy_current_state >= 15 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 12 );
+		while ( yy_base[yy_current_state] != 13 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -722,45 +725,51 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 5 "02aRecognizeIdentifiers.l"
+#line 7 "02aRecognizeIdentifiers.l"
 { a[0]++; }
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 6 "02aRecognizeIdentifiers.l"
+#line 8 "02aRecognizeIdentifiers.l"
 { a[1]++; }
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 7 "02aRecognizeIdentifiers.l"
+#line 9 "02aRecognizeIdentifiers.l"
 { a[2]++; }
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 8 "02aRecognizeIdentifiers.l"
+#line 10 "02aRecognizeIdentifiers.l"
 { a[3]++; }
 	YY_BREAK
 case 5:
 YY_RULE_SETUP
-#line 9 "02aRecognizeIdentifiers.l"
+#line 11 "02aRecognizeIdentifiers.l"
 { br_count++; }
 	YY_BREAK
 case 6:
 YY_RULE_SETUP
-#line 10 "02aRecognizeIdentifiers.l"
+#line 12 "02aRecognizeIdentifiers.l"
 { br_count--; }
 	YY_BREAK
 case 7:
 YY_RULE_SETUP
-#line 11 "02aRecognizeIdentifiers.l"
-{ id_count++; printf("\nIdentifier Found: %s", yytext); }
+#line 13 "02aRecognizeIdentifiers.l"
+{ id_count++; printf("Identifier Found: %s\n", yytext); }
 	YY_BREAK
 case 8:
+/* rule 8 can match eol */
 YY_RULE_SETUP
-#line 12 "02aRecognizeIdentifiers.l"
+#line 14 "02aRecognizeIdentifiers.l"
+{ return 0; }
+	YY_BREAK
+case 9:
+YY_RULE_SETUP
+#line 16 "02aRecognizeIdentifiers.l"
 ECHO;
 	YY_BREAK
-#line 764 "lex.yy.c"
+#line 773 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -953,7 +962,7 @@ static int yy_get_next_buffer (void)
 			{ /* Not enough room in the buffer - grow it. */
 
 			/* just a shorter name for the current buffer */
-			YY_BUFFER_STATE b = YY_CURRENT_BUFFER;
+			YY_BUFFER_STATE b = YY_CURRENT_BUFFER_LVALUE;
 
 			int yy_c_buf_p_offset =
 				(int) ((yy_c_buf_p) - b->yy_ch_buf);
@@ -1052,7 +1061,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 14 )
+			if ( yy_current_state >= 15 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -1080,13 +1089,13 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 14 )
+		if ( yy_current_state >= 15 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 13);
+	yy_is_jam = (yy_current_state == 14);
 
-	return yy_is_jam ? 0 : yy_current_state;
+		return yy_is_jam ? 0 : yy_current_state;
 }
 
     static void yyunput (int c, register char * yy_bp )
@@ -1174,7 +1183,7 @@ static int yy_get_next_buffer (void)
 				case EOB_ACT_END_OF_FILE:
 					{
 					if ( yywrap( ) )
-						return 0;
+						return EOF;
 
 					if ( ! (yy_did_buffer_switch_on_eof) )
 						YY_NEW_FILE;
@@ -1310,10 +1319,6 @@ static void yy_load_buffer_state  (void)
 	yyfree((void *) b  );
 }
 
-#ifndef __cplusplus
-extern int isatty (int );
-#endif /* __cplusplus */
-    
 /* Initializes or reinitializes a buffer.
  * This function is sometimes called more than once on the same buffer,
  * such as during a yyrestart() or at EOF.
@@ -1518,8 +1523,8 @@ YY_BUFFER_STATE yy_scan_string (yyconst char * yystr )
 
 /** Setup the input buffer state to scan the given bytes. The next call to yylex() will
  * scan from a @e copy of @a bytes.
- * @param bytes the byte buffer to scan
- * @param len the number of bytes in the buffer pointed to by @a bytes.
+ * @param yybytes the byte buffer to scan
+ * @param _yybytes_len the number of bytes in the buffer pointed to by @a bytes.
  * 
  * @return the newly allocated buffer state object.
  */
@@ -1527,7 +1532,8 @@ YY_BUFFER_STATE yy_scan_bytes  (yyconst char * yybytes, yy_size_t  _yybytes_len 
 {
 	YY_BUFFER_STATE b;
 	char *buf;
-	yy_size_t n, i;
+	yy_size_t n;
+	yy_size_t i;
     
 	/* Get memory for full buffer, including space for trailing EOB's. */
 	n = _yybytes_len + 2;
@@ -1757,15 +1763,18 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 12 "02aRecognizeIdentifiers.l"
+#line 15 "02aRecognizeIdentifiers.l"
 
 
-int main() {
+
+int main(int argc, char *argv[]) {
 	printf("Enter the expression: ");
+
 	yylex();
+
 	printf("\nOperator Count:\n");
-	printf("Addition: %d\nSubtraction: %d\nMultiplication: %d\nDivision: %d\n", a[0], a[1], a[2], a[3]);
-	printf("\nNumber of Identifiers: %d\n", id_count);
+	printf("Addition:\t%d\nSubtraction:\t%d\nMultiplication:\t%d\nDivision:\t%d\n", a[0], a[1], a[2], a[3]);
+	printf("Number of Identifiers:\t%d\n", id_count);
 
 	op_count = a[0] + a[1] + a[2] + a[3];
 
@@ -1773,7 +1782,7 @@ int main() {
 		printf("\nIt is an invalid expression!\n");
 	else
 		printf("\nIt is a valid expression!\n");
-	
+
 	return 0;
 }
 
