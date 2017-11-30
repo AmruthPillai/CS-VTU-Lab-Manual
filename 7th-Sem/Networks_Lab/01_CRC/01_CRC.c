b65060c76e791167f45dafc86a54d2a9c3ffb265
Amruth Pillai <im.amruth@gmail.com>
2017-11-30 17:26:46 +0530

    added .gitignore

--- 7th-Sem/Networks_Lab/01_CRC/01_CRC.c
+++ 7th-Sem/Networks_Lab/01_CRC/01_CRC.c
@@ -1,29 +1,26 @@
 #include <stdio.h>
 #include <string.h>
+
 #define N strlen(g)
 
 char t[128], cs[128], g[] = "100010000001000001";
 int a, e, c;
 
 void xor() {
-  for (c = 1; c < N; c++) {
+  for (c = 1; c < N; c++)
     cs[c] = (cs[c] == g[c]) ? '0' :'1';
-  }
 }
 
 void crc() {
-  for (e = 0; e < N; e++) {
+  for (e = 0; e < N; e++)
     cs[e] = t[e];
-  }
 
   do {
-    if (cs[0] == '1') {
+    if (cs[0] == '1')
       xor();
-    }
 
-    for (c = 0; c < N; c++) {
+    for (c = 0; c < N; c++)
       cs[c] = cs[c + 1];
-    }
 
     cs[c] = t[e++];
   } while (e < (a + N - 1));
@@ -37,19 +34,15 @@ int main(int argc, char const *argv[]) {
 
   a = strlen(t);
 
-  for (e = a; e < (a + N - 1); e++) {
+  for (e = a; e < (a + N - 1); e++)
     t[e] = '0';
-  }
 
   printf("Modified t[u]: %s\n", t);
-
   crc();
-
   printf("Checksum: %s\n", cs);
 
-  for (e = a; e < (a + N - 1); e++) {
+  for (e = a; e < (a + N - 1); e++)
     t[e] = cs[e - a];
-  }
 
   printf("Final Codeword: %s\n", t);
   printf("\nTest Error Detection? 0 for yes, 1 for no: ");
@@ -64,13 +57,12 @@ int main(int argc, char const *argv[]) {
 
   crc();
 
-  for (e = 0; (e < N - 1) && (cs[e] != '1'); e++) {
-    if (e < N - 1) {
-      printf("Error Detected!\n");
-    } else {
-      printf("No Error Detected!\n");
-    }
-  }
+  for (e = 0; (e < N - 1) && (cs[e] != '1'); e++);
+
+  if (e < N - 1)
+    printf("Error Detected!\n");
+  else
+    printf("No Error Detected!\n");
 
   return 0;
 }
