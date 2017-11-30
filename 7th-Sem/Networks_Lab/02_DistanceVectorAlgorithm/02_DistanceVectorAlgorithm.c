b0fd310c5e0eb703ffb9ff5f45ce387c9586c4d2
AmruthPillai <im.amruth@gmail.com>
2017-11-30 17:27:29 +0530

    updated PGM 2

--- 7th-Sem/Networks_Lab/02_DistanceVectorAlgorithm/02_DistanceVectorAlgorithm.c
+++ 7th-Sem/Networks_Lab/02_DistanceVectorAlgorithm/02_DistanceVectorAlgorithm.c
@@ -2,9 +2,7 @@
 
 struct network {
   int dist[10], outgoing[10];
-};
-
-struct network nodes[10];
+} nodes[10];
 
 void init(int n) {
   int i, j;
@@ -13,8 +11,59 @@ void init(int n) {
     for (j = 0; j < n; j++) {
       if (i != j) {
         nodes[i].dist[j] = 999;
-        nodes[i].outgoing[]
+        nodes[i].outgoing[j] = j;
+      }
+
+      nodes[i].dist[i] = 0;
+      nodes[i].outgoing[i] = i;
+    }
+  }
+}
+
+void update(int i, int j, int k) {
+  nodes[i].outgoing[j] = k;
+  nodes[i].dist[j] = nodes[i].dist[k] + nodes[k].dist[j];
+}
+
+void floyd(int n) {
+  int i, j, k;
+
+  for (k = 0; k < n; k++)
+    for (i = 0; i < n; i++)
+      for (j = 0; j < n; j++)
+        if (nodes[i].dist[j] > nodes[i].dist[k] + nodes[k].dist[j])
+          update(i, j, k);
+}
+
+int main(int argc, char const *argv[]) {
+  int n, i, j;
+
+  printf("Enter the number of nodes: ");
+  scanf("%d", &n);
+
+  init(n);
+
+  printf("Enter the distances:\n");
+
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n; j++) {
+      if (i != j) {
+        printf("Enter distance from %d to %d: ", i + 1, j + 1);
+        scanf("%d", &nodes[i].dist[j]);
       }
     }
   }
+
+  floyd(n);
+
+  printf("Distance Vector Routing Algorithm\n");
+  for (i = 0; i < n; i++) {
+    printf("%d Nodes Vector Table\n", i + 1);
+    printf("DEST\tDIST\tHOPS\n");
+    for (j = 0; j < n; j++)
+      if (i != j)
+        printf("%d\t%d\t%d\n", j + 1, nodes[i].dist[j], nodes[i].outgoing[j] + 1);
+  }
+
+  return 0;
 }
